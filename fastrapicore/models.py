af70703a66b9f2a85df0c4bfa1102883bf1c865b
shrutika <chavatshrutika@gmail.com>
2023-06-07 16:49:33 +0530

    updated

--- fastrapicore/models.py
+++ fastrapicore/models.py
@@ -5,3 +5,5 @@ class ProductRate(models.Model):
     tag_id = models.CharField(max_length=16,unique=True)
     title = models.CharField(max_length=255)
     rate = models.FloatField()
+    size = models.CharField(max_length=255, null=True)
+    image = models.CharField(max_length=255, null=True)
